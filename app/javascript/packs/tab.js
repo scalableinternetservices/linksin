6a59e8113bed333cf0e207d2435a076de7f4c8a5
Phipson <phipsonleecy@gmail.com>
2019-10-25 02:39:39 -0700

    Updated UI for Events, Messages, Swiping Cards

--- app/javascript/packs/tab.js
+++ app/javascript/packs/tab.js
@@ -9,12 +9,12 @@ window.openTab = function(evt, tabName) {
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("LI_tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.display = "grid";
     evt.currentTarget.className += " active";
 }
