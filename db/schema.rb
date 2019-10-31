282b6019859da92d15edaf97db55ccc41689fd62
Amy Tu <tu.amy.a@gmail.com>
2019-10-31 05:43:11 +0000

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_002621) do
+ActiveRecord::Schema.define(version: 2019_10_31_025232) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "send_id"
+    t.integer "recv_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "description"
@@ -26,20 +33,6 @@ ActiveRecord::Schema.define(version: 2019_10_31_002621) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_members_on_event_id"
     t.index ["user_id"], name: "index_members_on_user_id"
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer "send_id"
-    t.integer "recv_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "description"
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -72,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_10_31_002621) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "members", "events"
+  add_foreign_key "members", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
