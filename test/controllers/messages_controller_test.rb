d7c57955dcc2f8074aebb17e525fce2267afb744
Junhong Wang <ionejunhong@gmail.com>
2019-11-05 15:37:49 -0800

    refactor

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get messages_index_url
-    assert_response :success
-  end
 
 end
