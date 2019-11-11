abe4cc4dcc6396feb3fec942cfef8836c8e35106
Omar Tleimat <otleimat@gmail.com>
2019-11-10 20:05:43 -0800

    Added small initial code for seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def create_user(email,name)
+  User.create!(
+    email:  email,
+    name: name,
+    password: "password1234"
+  )
+end
+
+def create_event(title,description)
+  Event.create!(
+    title: title,
+    description: description
+  )
+end
+
+def range (min, max)
+    rand * (max-min) + min
+end
+
+1...10.times do |i|
+	create_user("user_#{i}@meow.com", Faker::Name.first_name)
+	create_event("Event #{i}", "Cool event")
+end
+
+User.all.each do |user|
+	Event.all.each do |event|
+		if range(0,1) < 0.33
+			user.events << event
+		end
+	end
+end
