65e0cf86db109656d9c6f431f38e92d8c17fef41
Phipson <phipsonleecy@gmail.com>
2019-11-22 02:27:09 -0800

    Updated caching and limiting card randomizations

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,13 @@ class Event < ApplicationRecord
 	has_many :members
     has_many :users, :through => :members
     validates_presence_of :description, :date, :time
+
+      # Event Caching
+    def Event.cache_key_for_event(e)
+        "event-#{e.id}-#{e.updated_at}-#{e.members.count}"
+    end
+
+    def Event.cache_key_for_eventList
+        "eventList-#{Event.maximum(:updated_at)}-#{Member.maximum(:updated_at)}"
+    end
 end
