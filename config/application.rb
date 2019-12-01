41313ff37f3541c945968ff70e627f7a6e1599b3
Junhong Wang <ionejunhong@gmail.com>
2019-12-01 14:58:55 -0800

    add database replica

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,7 @@ module Linksin
     # the framework and any gems in your application.
   end
 end
+
+config.active_record.database_selector = { delay: 2.seconds }
+config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
